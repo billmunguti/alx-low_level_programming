26a1c8190796bba7e0cdf4aa2ad768a2957730b0
billmunguti <williammunguti14@gmail.com>
2022-12-13 15:15:58 -0800

    ommission

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -8,11 +8,11 @@
 int main(void)
 {
 	char c;
-	
+
 	c = 'a';
 	while
 		(c <= 'z') {
-				if ((c != 'q' && c!= 'e') && c <= 'z')
+				if ((c != 'q' && c != 'e') && c <= 'z')
 					putchar(c);
 				c++;
 			}
