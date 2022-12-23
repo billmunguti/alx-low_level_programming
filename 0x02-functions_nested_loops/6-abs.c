0246e3a68232b7e0e2995ed610c3c1d4115d31d7
billmunguti <williammunguti14@gmail.com>
2022-12-23 06:12:30 -0800

    absolute

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _abs - prints absolute value of an integer
+ * @n: integer to be checked
+ * Return: absolute value of integer
+ */
+
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
+}
